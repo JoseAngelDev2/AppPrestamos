edb671bf53cc6a9f39d983a80b077774c589e539
JoseAngelDev2 <casuhud1@gmail.com>
2026-07-31 21:59:47 -0400

    Initial Project

---
+++ Core/BaseEntity.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppPrestamos.Domain.Core
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
