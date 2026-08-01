ff56a2d14ae818e58e656b0ee64680ea815767f6
JoseAngelDev2 <casuhud1@gmail.com>
2026-07-31 22:02:14 -0400

    Adding Entities

---
+++ Entities/Creditor.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppPrestamos.Domain.Core;
+
+namespace AppPrestamos.Domain.Entities
+{
+    public class Creditor : BaseEntity
+    {
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Type { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public int Age { get; set; }
+        public ICollection<Customer> Loans { get; set; } = new List<Customer>();
+    }
+}
